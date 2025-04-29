87dccffb03bbbdda6ec89e9c9710e21ff8dd5f16
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-04-29 21:05:20 +0530

    Python

---
+++ 2 Variables.py
@@ -0,0 +1,17 @@
+a="Sumit"
+b=10
+c=20.5
+d=True
+_d=10j
+d_d_d= 1
+print(a)
+print(b)
+print(c)
+print(d)
+print(_d)
+print(d_d_d)
+
+# 1  Variable names can only start with a letter or an underscore
+# 2  Variable names can only contain letters, numbers, and underscores
+# 3  Variable names cannot be a reserved keyword in Python
+# 4  Variable names are case-sensitive
